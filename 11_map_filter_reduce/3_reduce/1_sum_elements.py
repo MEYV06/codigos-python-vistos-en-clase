0e952df6c6bb9584eccdc7573aa09c2f84b9e092
rgap <r.guzmanap@gmail.com>
2026-07-02 08:15:03 -0500

    Stop tracking map filter reduce exercises

--- 11_map_filter_reduce/3_reduce/1_sum_elements.py
+++
@@ -1,6 +0,0 @@
-# Ejemplo: Utilizar reduce para sumar todos los elementos en una lista
-from functools import reduce
-
-numbers = [1, 2, 3, 4, 5]
-total_sum = reduce(lambda x, y: x + y, numbers)
-print('Sum of all elements:', total_sum)
--- 11_map_filter_reduce/3_reduce/3_concatenate_strings.py
+++
@@ -1,6 +0,0 @@
-# Ejemplo: Utilizar reduce para concatenar una lista de strings
-from functools import reduce
-
-strings = ["{apple}", "{banana}", "Frutas:"]
-concatenated_string = reduce(lambda x, y: x + y, strings)
-print('Concatenated string:', concatenated_string)
