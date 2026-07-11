6cd36eccbcb7c6f831abf3747d6b25a174055981
rgap <r.guzmanap@gmail.com>
2026-07-11 00:07:54 -0500

    refactor: reorganize modules and namespaces, update class examples, and consolidate import logic structure

--- 16_classes_objects/08_class_methods.py
+++ 16_classes_objects/08_class_methods.py
@@ -3,11 +3,21 @@
 # Metodos de clase
 # Un metodo de clase recibe la clase como primer parametro, normalmente llamado cls.
 # Se define usando el decorador @classmethod.
+#
+# Diferencia con un metodo normal:
+# metodo normal -> recibe self y puede acceder a atributos del objeto.
+# @classmethod   -> recibe cls y puede acceder a atributos de la clase.
 
 
 class Student:
     school_name = "Escuela de Python"
     total_students = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print("Estudiante:", self.name)
 
     @classmethod
     def register_student(cls):
@@ -19,15 +29,33 @@ class Student:
         cls.school_name = new_school_name
 
 
+# Metodo normal: trabaja con un objeto especifico.
+# Puede acceder a atributos creados con self, como self.name.
+student_1 = Student("Ana")
+student_1.introduce()
+
 # Llamar a metodos de clase desde la clase
 Student.register_student()
 Student.register_student()
 Student.change_school("Escuela Pythonista")
 
 print("Escuela:", Student.school_name)
+
+# Evita llamar metodos de clase desde objetos.
+# Ejemplo de algo permitido, pero no recomendado:
+# student_1.register_student()
+# Aunque Python lo permite, en estos ejemplos los llamaremos desde la clase
+# porque trabajan con datos de la clase, no con datos de un objeto especifico.
+#
+# Un metodo de clase no puede acceder directamente a self.name,
+# porque recibe cls, no self.
+
 # Salidas:
+# Estudiante: Ana
 # Estudiantes registrados: 1
 # Estudiantes registrados: 2
 # Escuela: Escuela Pythonista
 
 # Nota: Los metodos de clase se usan cuando el metodo necesita trabajar con la clase.
+# Por claridad, se recomienda llamarlos desde la clase:
+# Student.register_student()
--- 16_classes_objects/09_static_methods.py
+++ 16_classes_objects/09_static_methods.py
@@ -3,9 +3,27 @@
 # Metodos estaticos
 # Un metodo estatico no recibe self ni cls.
 # Se define usando el decorador @staticmethod.
+#
+# Diferencia rapida:
+# metodo normal -> recibe self y trabaja con datos de un objeto.
+# @classmethod  -> recibe cls y puede trabajar con datos de la clase.
+# @staticmethod -> no recibe cls ni self; solo usa los argumentos que le pasas.
 
 
 class Student:
+    total_students = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        print("Hola, soy", self.name)
+
+    @classmethod
+    def register_student(cls):
+        cls.total_students += 1
+        print("Estudiantes registrados:", cls.total_students)
+
     @staticmethod
     def is_valid_grade(grade):
         return 0 <= grade <= 20
@@ -15,12 +33,33 @@ class Student:
         return (grade_1 + grade_2) / 2
 
 
-# Llamar a metodos estaticos desde la clase
+# metodo normal: trabaja con un objeto especifico porque usa self.name
+student_1 = Student("Ana")
+student_1.introduce()
+
+# classmethod: trabaja con la clase porque modifica Student.total_students
+Student.register_student()
+Student.register_student()
+
+# staticmethod: no trabaja con la clase ni con un objeto.
+# Solo usa los valores que recibe como argumentos.
 print("Nota valida:", Student.is_valid_grade(18))
 print("Promedio:", Student.average(16, 18))
+
 # Salidas:
+# Hola, soy Ana
+# Estudiantes registrados: 1
+# Estudiantes registrados: 2
 # Nota valida: True
 # Promedio: 17.0
 
 # Nota: Los metodos estaticos se usan cuando la funcion pertenece a la clase,
 # pero no necesita acceder a atributos del objeto ni de la clase.
+#
+# Tambien se puede acceder a un metodo estatico desde un objeto:
+# student_1.is_valid_grade(18)
+#
+# Python lo permite, pero normalmente es mas claro llamarlo desde la clase:
+# Student.is_valid_grade(18)
+#
+# Python prioriza la flexibilidad en vez de prohibir usos que pueden ser confusos.
